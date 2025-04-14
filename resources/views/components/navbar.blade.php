39b36454f6a93715cc3d983228ec0c9a1317740f
Eryk Ross <82853156+XenoRoss@users.noreply.github.com>
2025-04-14 12:29:19 -0400

    Updated Navbar Layout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 @props(['showCreate' => false])
 
-<nav class="fixed top-0 left-0 right-0 w-full overflow-x-hidden z-50 h-16">
+<nav class="fixed top-0 left-0 right-0 w-full overflow-x-hidden z-50 h-24">
     <div class="bg-gray-200 dark:bg-gray-800 transition-colors duration-300 w-full h-full">
         <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 h-full">
             <!-- Outer flex container spans the full height -->
@@ -13,7 +13,7 @@
                 </a>
                 
                 <!-- Right side: Create, Blog, Hamburger -->
-                <div class="flex items-center space-x-6">
+                <div class="flex items-center gap-4">
                     
                     @if($showCreate)
                         @auth
